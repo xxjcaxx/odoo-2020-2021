e1aa40005f4a7a0ea09e85e44fff3cd51f4863d5
xxjcaxx <jocasal@gmail.com>
2020-10-02 07:42:10 +0000

    divendres fields relacionals

--- tasks/models/models.py
+++ tasks/models/models.py
@@ -7,13 +7,20 @@ class task(models.Model):
     _name = 'tasks.task'
     _description = 'Les tasques'
 
-    name = fields.Char()
+    name = fields.Char(string="Tasca",required=True)
     date = fields.Datetime()
     done = fields.Boolean()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    photo = fields.Image(max_width=200)
+    priority = fields.Selection([('1','Low'),('2','Normal'),('3','High')])
+    student = fields.Many2one('tasks.student',ondelete='cascade',required=True)
+    group = fields.Many2many('tasks.student')
+
+class student(models.Model):
+    _name = 'tasks.student'
+    _description = 'Estudiants'
+
+    name = fields.Char(required=True)
+    tasks = fields.One2many('tasks.task','student')
+    group_task = fields.Many2many('tasks.task')
+
+
