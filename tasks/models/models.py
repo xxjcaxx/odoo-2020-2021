a7668555a71e1b3dc352d6ab759249a7248467a8
xxjcaxx <jocasal@gmail.com>
2020-10-05 06:37:34 +0000

    many2many

--- tasks/models/models.py
+++ tasks/models/models.py
@@ -13,7 +13,11 @@ class task(models.Model):
     photo = fields.Image(max_width=200)
     priority = fields.Selection([('1','Low'),('2','Normal'),('3','High')])
     student = fields.Many2one('tasks.student',ondelete='cascade',required=True)
-    group = fields.Many2many('tasks.student')
+    group = fields.Many2many(comodel_name='tasks.student',
+                             relation='task_student_group',
+                             column1='task_id', column2='student_id')
+    cor_group = fields.Many2many(comodel_name='tasks.student',
+                                 relation ='task_student_cor')
 
 class student(models.Model):
     _name = 'tasks.student'
@@ -21,6 +25,10 @@ class student(models.Model):
 
     name = fields.Char(required=True)
     tasks = fields.One2many('tasks.task','student')
-    group_task = fields.Many2many('tasks.task')
+    group_task = fields.Many2many(comodel_name = 'tasks.task',
+                                  relation = 'task_student_group',
+                                  column1 = 'student_id', column2 = 'task_id')
+    cor_task = fields.Many2many(comodel_name='tasks.task',
+                                relation = 'task_student_cor')
 
 
