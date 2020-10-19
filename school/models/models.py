f3a2ee525de3938b769dd6a136fb7c273ffc14c8
xxjcaxx <jocasal@gmail.com>
2020-10-19 07:11:36 +0000

    related

--- school/models/models.py
+++ school/models/models.py
@@ -8,5 +8,15 @@ class course(models.Model):
      _description = 'Courses'
 
      name = fields.Char()
-     students = fields.Many2many('res.partner')
+     students = fields.One2many('school.student', 'course')
+     country = fields.Many2one('res.country')
 
+
+class student(models.Model):
+     _name = 'school.student'
+     _description = 'Students'
+
+     name = fields.Char()
+     course = fields.Many2one('school.course')
+     course_name = fields.Char(string='Course Name',related='course.name')
+     course_country = fields.Char(string='Course country', related='course.country.name')
