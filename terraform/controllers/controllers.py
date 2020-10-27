ce96248428eec403c6ab0a47ad8b281e241f4f4b
xxjcaxx <jocasal@gmail.com>
2020-10-27 19:31:17 +0000

    millores controllers

--- terraform/controllers/controllers.py
+++ terraform/controllers/controllers.py
@@ -7,25 +7,42 @@ class Terraform(http.Controller):
      def index(self, **kw):
          return "Hello, world"
 
-     @http.route('/terraform/terraform/planets/', auth='public', cors='*' )
-     def list(self, **kw):
+
+     @http.route('/terraform/terraform/<model>/<obj>/<field>', auth='public',cors='*', type='json')
+     def terraform_field(self, model, obj, field, **kw):
+         model = http.request.env[model].sudo().search([('id','=',obj)]).mapped(lambda p: p.read([field])[0])
+         return model
+
+     @http.route('/terraform/terraform/<model>/<obj>', auth='public',cors='*', type='json')
+     def terraform_object(self, model, obj, **kw):
+         print(obj+"**************************")
+         model = http.request.env[model].sudo().search([('id','=',obj)]).mapped(lambda p: p.read()[0])
+         return model
+
+     @http.route('/terraform/terraform/<model>', auth='public',cors='*', type='json')
+     def terraform_model(self, model, **kw):
+         model = http.request.env[model].sudo().search([]).mapped(lambda p: p.read()[0])
+         return model
+
+     @http.route('/terraform/terraform/players/<model("terraform.player"):obj>/', auth='public',cors='*')
+     def player(self, obj, **kw):
+         player = http.request.env['terraform.player'].sudo().search([('id','=',obj.id)]).mapped(lambda p: {'id':p.id, 'name': p.name, 'avatar':str(p.avatar)})
+         #print(player)
+         return json.dumps(player)
+
+     @http.route('/terraform/terraform/players/<model("terraform.player"):obj>/<field>', auth='public',cors='*')
+     def player_field(self, obj, field, **kw):
+         player = http.request.env['terraform.player'].sudo().search([('id','=',obj.id)]).mapped(lambda p: {field: str(p[field])})
+         #print(player)
+         return json.dumps(player)
+
+     @http.route('/terraform/terraform/planets/<limit>', auth='public', cors='*', type="json", csrf=False )
+     def list(self, limit, **kw):
       # https://stackoverflow.com/questions/51149008/odoo-export-res-company-object-to-json
-         planets = http.request.env['terraform.planet'].sudo().search([]).mapped(lambda p: {'name': p.name,
-                                                                                            'sun': p.sun.id,
-                                                                                            'player' :p.player.id,
-                                                                                            'n_player': p.n_planet,
-                                                                                            'average_temperature': p.average_temperature,
-                                                                                            'oxigen': p.oxigen,
-                                                                                            'co2': p.co2,
-                                                                                            'water': p.water,
-                                                                                            'gravity': p.gravity,
-                                                                                            'air_density': p.air_density,
-                                                                                            'material': p.material,
-                                                                                            'buildings': p.buildings.ids
-                                                                                            })
+         planets = http.request.env['terraform.planet'].sudo().search([],limit=int(limit)).mapped(lambda p: p.read()[0]) 
          #planets = {'planets':planets}
          #print(planets)
-         planets = json.dumps(planets)
+         #planets = json.dumps(planets)
          return planets
 
        #  return http.request.render('terraform.listing', {
@@ -33,11 +50,11 @@ class Terraform(http.Controller):
        #      'objects': http.request.env['terraform.planet'].sudo().search([]),   # Lo del sudo a solucionar con autentificacion
        #  })
 
-     @http.route('/terraform/terraform/planets/<model("terraform.planet"):obj>/', auth='public',cors='*')
-     def object(self, obj, **kw):
-         return http.request.render('terraform.object', {
-             'object': obj
-         })
+     @http.route('/terraform/terraform/planet/<model("terraform.planet"):obj>/', auth='public',cors='*', type='json', csrf=False)
+     def planet(self, obj, **kw):
+         print(obj)
+         planet = http.request.env['terraform.planet'].sudo().search([('id','=',obj.id)]).mapped(lambda p: p.read()[0])
+         return planet
 
     # @http.route('/web/session/authenticate', auth="none", cors='*')
     # def authenticate(self, db, login, password, base_location=None):
