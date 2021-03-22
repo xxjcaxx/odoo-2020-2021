4857ee99e13b52e6425b25efe96728df5252ed02
xxjcaxx <jocasal@gmail.com>
2021-03-22 08:50:23 +0000

    sales

--- simulator/models/models.py
+++ simulator/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import random
 
 
 class simulator(models.Model):
@@ -18,11 +19,10 @@ class simulator(models.Model):
      def simular(self):
          print('simular')
 
-
      @api.model
      def create(self, values):
-        record = super(simulator, self).create(values)
-        cron = self.env['ir.cron'].create({
+       record = super(simulator, self).create(values)
+       cron = self.env['ir.cron'].create({
             'name': record.name,
             'model_id': self.env.ref('simulator.model_simulator_simulator').id,
             'state': 'code',
@@ -34,8 +34,8 @@ class simulator(models.Model):
             'activity_user_type': 'specific',
             'doall': False
              })
-        record.write({'cron': cron.id})
-        return record
+       record.write({'cron': cron.id})
+       return record
 
      def unlink(self):
          print('**************************'+str(self))
@@ -45,4 +45,31 @@ class simulator(models.Model):
      @api.model
      def run_simulation(self,id):
          print("Simulation "+str(id))
+         simulator = self.search([('id','=',id)])
+         probability = 1/simulator.average_period
+         if random.random() < probability:
+             print('Doing simulation')
+             if simulator.type == 'sales':
+                 client = self.env['res.partner'].search([('customer_rank','>',0)])
+                 if len(client) > 0:
+                     client = self.env['res.partner'].browse(random.choice(client.ids))
+                     print(client.name)
+                     order = self.env['sale.order'].create({
+                         'partner_id': client.id
+                     })
+                     product_quantity = random.randint(1,6)
+                     for i in range(0,product_quantity):
+                         product = self.env['product.product'].browse(random.choice(self.env['product.product'].search([]).ids))
+                         print(i,product)
+                         order_line = self.env['sale.order.line'].create({
+                             'order_id': order.id,
+                             'product_id': product.id,
+                             'product_uom_qty': random.randint(1,100)
+                         })
+                         print(order_line)
+
+
+
+
+
 
